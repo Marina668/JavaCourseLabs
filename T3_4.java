5ea4a17ec47d9387b1a382f76a4b7cab7f956215
Maryna Kostiuchenko <69366479+Marina668@users.noreply.github.com>
2022-09-26 21:16:19 +0300

    Kostiuchenko Maryna Lab 2
    tasks 3.1-3.8, 4.4, 3.20, 4.6в

---
+++ T3_4.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class T3_4 {
+    public static void main(String[] args) {
+        float a = 1e-4f;
+        double b = 2.33E5;
+        double c = Math.PI;
+        double d = Math.E;
+        double e = Math.sqrt(5);
+        double f = Math.log(100);
+        System.out.printf("%.4f %f %f %f %f %f", a, b, c, d, e, f);
+
+    }
+}
