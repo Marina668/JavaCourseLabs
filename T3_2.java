7008078d34087d555512ce46663aad4e4a7785fe
Maryna Kostiuchenko <69366479+Marina668@users.noreply.github.com>
2022-10-25 19:45:00 +0300

    Kostiuchenko Maryna HW6
    tasks 4.2.6, 4.3.2

---
+++ T3_2.java
@@ -0,0 +1,9 @@
+package mypack_t_3_2;
+
+public class T3_2 {
+    public static void main(String[] args) {
+        ComplexNumber numbers = new ComplexNumber(6, new int[]{3, 2, -2, -2, 5, -7});
+        numbers.Sum();
+        numbers.Multiply();
+    }
+}
